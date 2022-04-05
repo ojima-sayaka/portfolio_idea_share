bc244852e33639f8e5bd95e270ff0b4d2a95bd38
ojima-sayaka <perfect318sun@gmail.com>
2022-04-05 06:25:41 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-# Top
-root to: "public/homes#top"
-get "public/homes#about"
+# ゲスト・会員　Top
+  root "public/homes#top"
+  get 'about', to: "public/homes#about"
+
+
+
+
+
+
+#　管理者 Top
+  root "admin/homes#top"
+  
 
 end
