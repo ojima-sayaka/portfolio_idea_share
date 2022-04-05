8a99a75a8ffbc63f29da0b1be40c203ecf41a440
ojima-sayaka <perfect318sun@gmail.com>
2022-04-05 06:45:47 +0000

    homes admin/menbers top OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-# ゲスト・会員　Top
-  root "public/homes#top"
-  get 'about', to: "public/homes#about"
+# Top
+root "public/homes#top"
+get 'about', to: "public/homes#about"
 
+# member
+# search
 
+# 管理者
+namespace :admin do
+  root "homes#top"
 
-
-
-
-#　管理者 Top
-  root "admin/homes#top"
-  
+end
+#devise
+ # devise_for :members,skip:[:passwords], controllers: {registrations: "public/registrations",sessions: 'public/sessions'}
+ # devise_for :admin,skip:[:registrations, :passwords], controllers: {sessions: "admin/sessions"}
 
 end
