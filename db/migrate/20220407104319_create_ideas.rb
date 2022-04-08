2ad4ed7feb4652771c58949998bcca6bdc990fe4
ojima-sayaka <perfect318sun@gmail.com>
2022-04-08 07:50:36 +0000

    ideas title カラム追加

--- db/migrate/20220407104319_create_ideas.rb
+++ db/migrate/20220407104319_create_ideas.rb
@@ -3,7 +3,8 @@ class CreateIdeas < ActiveRecord::Migration[6.1]
     create_table :ideas do |t|
       t.integer :category_id, null: false
       t.integer :member_id, null: false
-      t.string :idea_detail, null: false
+      t.string  :title, null: false
+      t.text :idea_detail, null: false
       t.text :image
       t.timestamps
     end
