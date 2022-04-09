fd2fdc895e3e266bfbbdf01a72d07d525fcec67e
ojima-sayaka <perfect318sun@gmail.com>
2022-04-09 10:55:52 +0000

    checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,28 @@
 Rails.application.routes.draw do
+
+  namespace :members do
+    get 'good/index'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#devise
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
+  devise_for :members, controllers: {
+    sessions: 'members/sessions'
+  }
 
-# Top
-root "public/homes#top"
-get 'about', to: "public/homes#about"
-
-# devise_scope :members do
-#   post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-# end
-
+# top
+  root "members/home#top"
+    get "/about", to: "members/home#about", as: "about"
 # member
-namespace :members do
-  resources :ideas
-  resources :members
+  namespace :members do
+    resources :ideas
+    resources :members
+  end
 
 #search
-get "search" => "searches#search"
-get "search_result" => "searches#search_result"
-end
+
 
 # admin
 namespace :admin do
@@ -28,10 +33,7 @@ namespace :admin do
 
 end
 
-# アプリケーションを作ろう②5章　以前のチーム開発　参考
 
-#devise
-devise_for :members,skip:[:passwords], controllers: {registrations: "public/devise/registrations",sessions: 'public/sessions'}
-devise_for :admin,skip:[:registrations, :passwords], controllers: {sessions: "admin/sessions"}
+
 
 end
